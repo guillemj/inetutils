1965afb227ff00db56d822c4848bbb3f16f10c44
Simon Josefsson <simon@josefsson.org>
2021-07-26 07:19:35 +0200

    logger: Code for non-getaddrinfo removed.
    * NEWS: Mention change.
    * src/logger.c [!HAVE_DECL_GETADDRINFO]: Remove.

--- src/logger.c
+++ src/logger.c
@@ -61,14 +61,12 @@ static char *unixsock = NULL;
 static char *source;
 static char *pidstr;
 
-#if HAVE_DECL_GETADDRINFO
-# if HAVE_IPV6
+#if HAVE_IPV6
 static int host_family = AF_UNSPEC;
-# else
+#else
 /* Fall back to only IPv4.  */
 static int host_family = AF_INET;
-# endif /* !HAVE_IPV6 */
-#endif /* HAVE_DECL_GETADDRINFO */
+#endif /* !HAVE_IPV6 */
 
 
 
@@ -141,9 +139,7 @@ open_socket (void)
   union logger_sockaddr sockaddr;
   socklen_t socklen;
   int family;
-#if HAVE_DECL_GETADDRINFO
   int ret;
-#endif
 
   /* A UNIX socket name can be specified in two ways.
    * Zero length of `unixsock' is handled automatically.  */
@@ -165,13 +161,7 @@ open_socket (void)
     }
   else
     {
-#if HAVE_DECL_GETADDRINFO
       struct addrinfo hints, *ai, *res;
-#else
-      struct hostent *hp;
-      struct servent *sp;
-      unsigned short port;
-#endif /* !HAVE_DECL_GETADDRINFO */
       char *p;
 
 #if HAVE_IPV6
@@ -209,7 +199,6 @@ open_socket (void)
       if (!p)
 	p = "syslog";
 
-#if HAVE_DECL_GETADDRINFO
       memset (&hints, 0, sizeof (hints));
       hints.ai_socktype = SOCK_DGRAM;
 
@@ -275,43 +264,13 @@ open_socket (void)
 	error (EXIT_FAILURE, EADDRNOTAVAIL, "%s:%s", host, p);
 
       /* Existing socket can be returned now.
-       * This handles AF_INET and AF_INET6 in case
-       * HAVE_DECL_GETADDRINFO is true.  */
+       * This handles AF_INET and AF_INET6.  */
       return;
-
-#else /* !HAVE_DECL_GETADDRINFO */
-
-      sockaddr.sinet.sin_family = AF_INET;
-      family = PF_INET;
-
-      hp = gethostbyname (host);
-      if (hp)
-	sockaddr.sinet.sin_addr.s_addr = *(unsigned long*) hp->h_addr_list[0];
-      else if (inet_aton (host, (struct in_addr *) &sockaddr.sinet.sin_addr)
-	       != 1)
-	error (EXIT_FAILURE, 0, "unknown host name");
-
-      if (isdigit (*p))
-	{
-	  char *end;
-	  unsigned long n = strtoul (p, &end, 10);
-	  if (*end || (port = n) != n)
-	    error (EXIT_FAILURE, 0, "%s: invalid port number", p);
-	  port = htons (port);
-	}
-      else if ((sp = getservbyname (p, "udp")) != NULL)
-	port = sp->s_port;
-      else
-	error (EXIT_FAILURE, 0, "%s: unknown service name", p);
-
-      sockaddr.sinet.sin_port = port;
-#endif /* !HAVE_DECL_GETADDRINFO */
 
       socklen = sizeof (sockaddr.sinet);
     }
 
-  /* Execution arrives here for AF_UNIX and for
-   * situations with !HAVE_DECL_GETADDRINFO.  */
+  /* Execution arrives here for AF_UNIX.  */
 
   fd = socket (family, SOCK_DGRAM, 0);
   if (fd < 0)
